9f322f84c9ceb9971ea8eb586bda56927c014e79
yungtiec <tctammychu@gmail.com>
2018-03-26 13:27:40 -0400

    test role and permission

--- script/seed.js
+++ script/seed.js
@@ -1,12 +1,12 @@
 const db = require("../server/db");
-const { User, Permission, Role } = require("../server/db/models");
+const { User, Permission, Role, Project } = require("../server/db/models");
 Promise = require("bluebird");
 
 async function seed() {
   await db.sync({ force: true });
   console.log("db synced!");
+  await seedPermission();
   await seedUser();
-  await seedPermission()
 }
 
 async function seedUser() {
@@ -15,33 +15,62 @@ async function seedUser() {
     User.create({ email: "murphy@email.com", password: "123" })
   ]);
 
+  const projects = await seedProject();
+  await users[0].addProject(projects[0].id);
+  await users[1].addProject(projects[1].id);
+
+  const adminRole = await Role.findOne({ where: { name: "admin" } });
+  await users[0].addRole(adminRole.id)
+
   console.log(`seeded ${users.length} users`);
   console.log(`seeded successfully`);
 }
 
 async function seedPermission() {
-  const permissions = await Promise.map([
-    { name: "upvote" },
-    { name: "annotate" },
-    { name: "answer_survey" },
-    { name: "create_survey" },
-    { name: "approve_survey_answers" },
-    { name: "approve_annotation" }
-  ], entry => Permission.create(entry));
-  const role = await Promise.map([
-    { name: "admin" },
-    { name: "open_source_user" },
-    { name: "business_user" }
-  ], entry => Role.create(entry))
+  const permissions = await Promise.map(
+    [
+      { name: "upvote" },
+      { name: "annotate" },
+      { name: "answer_survey" },
+      { name: "create_survey" },
+      { name: "approve_survey_answers" },
+      { name: "approve_annotation" }
+    ],
+    entry => Permission.create(entry)
+  );
+  const role = await Promise.map(
+    [
+      { name: "admin" },
+      { name: "open_source_user" },
+      { name: "business_user" }
+    ],
+    entry => Role.create(entry)
+  );
   await Promise.map(permissions, permission => {
-    return role[0].addPermission(permission.id)
-  })
+    return role[0].addPermission(permission.id);
+  });
   await Promise.map(permissions.slice(0, 2), permission => {
-    return role[1].addPermission(permission.id)
-  })
+    return role[1].addPermission(permission.id);
+  });
   await Promise.map(permissions.slice(0, 4), permission => {
-    return role[2].addPermission(permission.id)
-  })
+    return role[2].addPermission(permission.id);
+  });
+}
+
+async function seedProject() {
+  return Promise.map(
+    [
+      {
+        name: "hello coin",
+        symbol: "HC"
+      },
+      {
+        name: "digital gold",
+        symbol: "DG"
+      }
+    ],
+    project => Project.create(project)
+  );
 }
 
 seed()
--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -1,35 +1,90 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const db = require('../index')
-const User = db.model('user')
+const { expect } = require("chai");
+const db = require("../index");
+const User = db.model("user");
+const Role = db.model("role");
+const Permission = db.model("permission");
+Promise = require("bluebird");
 
-describe('User model', () => {
+describe("User model", () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    // return db.sync({ force: true });
+  });
 
-  describe('instanceMethods', () => {
-    describe('correctPassword', () => {
-      let cody
+  describe("instanceMethods", () => {
+    describe("correctPassword", () => {
+      let cody;
 
-      beforeEach(() => {
+      before(() => {
         return User.create({
-          email: 'cody@puppybook.com',
-          password: 'bones'
+          email: "cody@puppybook.com",
+          password: "bones"
+        }).then(user => {
+          cody = user;
+        });
+      });
+
+      it("returns true if the password is correct", () => {
+        expect(cody.correctPassword("bones")).to.be.equal(true);
+      });
+
+      it("returns false if the password is incorrect", () => {
+        expect(cody.correctPassword("bonez")).to.be.equal(false);
+      });
+    }); // end describe('correctPassword')
+
+    describe("role and permission", async () => {
+      let permissions, role, cody;
+      before(async () => {
+        cody = await User.findOne({
+          where: { email: "cody@puppybook.com" }
+        });
+
+        permissions = await Promise.map(
+          [
+            { name: "upvote" },
+            { name: "annotate" },
+            { name: "answer_survey" },
+            { name: "create_survey" },
+            { name: "approve_survey_answers" },
+            { name: "approve_annotation" }
+          ],
+          entry => Permission.create(entry)
+        );
+        roles = await Promise.map(
+          [
+            { name: "admin" },
+            { name: "open_source_user" },
+            { name: "business_user" }
+          ],
+          entry => Role.create(entry)
+        );
+        await Promise.map(permissions, permission => {
+          return roles[0].addPermission(permission.id);
+        });
+        await Promise.map(permissions.slice(0, 2), permission => {
+          return roles[1].addPermission(permission.id);
+        });
+        await Promise.map(permissions.slice(0, 4), permission => {
+          return roles[2].addPermission(permission.id);
+        });
+        return await cody.addRole(roles[0].id);
+      });
+
+      it("returns role and permission info", async () => {
+        cody = await User.findOne({
+          where: { email: "cody@puppybook.com" },
+          include: [{
+            model: Role,
+            include: [{
+              model: Permission
+            }]
+          }]
         })
-          .then(user => {
-            cody = user
-          })
-      })
-
-      it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true)
-      })
-
-      it('returns false if the password is incorrect', () => {
-        expect(cody.correctPassword('bonez')).to.be.equal(false)
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
-}) // end describe('User model')
+        expect(cody.toJSON().roles[0].name).to.be.equal('admin');
+        expect(cody.toJSON().roles[0].permissions.length).to.be.equal(6);
+      });
+    });
+  }); // end describe('instanceMethods')
+}); // end describe('User model')
