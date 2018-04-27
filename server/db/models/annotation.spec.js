f104c6880c5798fbfced6235cf26d364d1fc33b7
yungtiec <tctammychu@gmail.com>
2018-04-27 16:09:08 +0800

    fix

--- server/db/models/annotation.spec.js
+++ server/db/models/annotation.spec.js
@@ -9,24 +9,18 @@ describe("Annotation thread", () => {
     await db.sync({ force: true });
     parent = await Annotation.create({
       uri: "http://localhost:8080/project/DG/survey/3",
-      survey_question_id: "12",
-      project_survey_id: "3",
       quote:
         "nunc eget orci malesuada, a tincidunt nunc sagittis. Donec faucibus fermentum placerat. Ut bibendum purus vel viverra vehicula. Ut in arcu vitae turpis rhoncus fermentum. Integer varius malesuada dictum. Vestibulum placerat auctor eros in luctus. Ut at sem arcu. Sed commodo maximus malesuada. Morbi consequat lectus felis, eu malesuada metus placerat vitae. In porta dui non odio efficitur, at congue leo dapibus. Suspendisse potenti.",
       text: "parent"
     });
     child = await Annotation.create({
       uri: "http://localhost:8080/project/DG/survey/3",
-      survey_question_id: "12",
-      project_survey_id: "3",
       quote:
         "nunc eget orci malesuada, a tincidunt nunc sagittis. Donec faucibus fermentum placerat. Ut bibendum purus vel viverra vehicula. Ut in arcu vitae turpis rhoncus fermentum. Integer varius malesuada dictum. Vestibulum placerat auctor eros in luctus. Ut at sem arcu. Sed commodo maximus malesuada. Morbi consequat lectus felis, eu malesuada metus placerat vitae. In porta dui non odio efficitur, at congue leo dapibus. Suspendisse potenti.",
       text: "child"
     });
     grandChild = await Annotation.create({
       uri: "http://localhost:8080/project/DG/survey/3",
-      survey_question_id: "12",
-      project_survey_id: "3",
       quote:
         "nunc eget orci malesuada, a tincidunt nunc sagittis. Donec faucibus fermentum placerat. Ut bibendum purus vel viverra vehicula. Ut in arcu vitae turpis rhoncus fermentum. Integer varius malesuada dictum. Vestibulum placerat auctor eros in luctus. Ut at sem arcu. Sed commodo maximus malesuada. Morbi consequat lectus felis, eu malesuada metus placerat vitae. In porta dui non odio efficitur, at congue leo dapibus. Suspendisse potenti.",
       text: "grand child"
@@ -51,10 +45,13 @@ describe("Annotation thread", () => {
       }
     });
 
-    var parent = rootAnnotation.toJSON()
-    var child = rootAnnotation.toJSON().children[0].toJSON()
-    var grandChild = rootAnnotation.toJSON().children[0].toJSON().children[0].toJSON()
-    expect(parent.id).to.be.equal(child.parentId)
-    expect(child.id).to.be.equal(grandChild.parentId)
+    var parent = rootAnnotation.toJSON();
+    var child = rootAnnotation.toJSON().children[0].toJSON();
+    var grandChild = rootAnnotation
+      .toJSON()
+      .children[0].toJSON()
+      .children[0].toJSON();
+    expect(parent.id).to.be.equal(child.parentId);
+    expect(child.id).to.be.equal(grandChild.parentId);
   });
 });
