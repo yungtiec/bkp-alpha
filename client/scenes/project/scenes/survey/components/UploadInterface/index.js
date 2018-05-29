07c6c3ba29741f0c9336c1409b88eefcfa5995eb
yungtiec <tctammychu@gmail.com>
2018-05-29 17:11:36 +0800

    preview document difference

--- client/scenes/project/scenes/survey/components/UploadInterface/index.js
+++ client/scenes/project/scenes/survey/components/UploadInterface/index.js
@@ -5,6 +5,22 @@ import autoBind from "react-autobind";
 import { SurveyHeader } from "../index";
 import Dropzone from "react-dropzone";
 import ReactMarkdown from "react-markdown";
+import Diff from "text-diff";
+import sanitizeHtml from "sanitize-html";
+
+function getSurveyMarkdown({ surveyTitle, surveyQnaIds, surveyQnasById }) {
+  const newline = "\n\n";
+  var surveyMarkdown = "# " + surveyTitle + newline;
+  surveyQnaIds.forEach(sid => {
+    surveyMarkdown += surveyQnasById[sid].question.markdown;
+    surveyMarkdown += surveyQnasById[sid].project_survey_answers[0].markdown;
+    if (surveyQnasById[sid].project_survey_answers[0].children.length)
+      surveyQnasById[sid].project_survey_answers[0].children.forEach(child => {
+        surveyMarkdown += child.markdown;
+      });
+  });
+  return surveyMarkdown;
+}
 
 export default class UploadInterface extends Component {
   constructor(props) {
@@ -19,6 +35,7 @@ export default class UploadInterface extends Component {
         uploadedMarkdown: self.fileReader.result
       });
     };
+    this.diff = new Diff();
   }
 
   onDrop(file) {
@@ -36,6 +53,15 @@ export default class UploadInterface extends Component {
       surveyMetadata,
       projectMetadata
     } = this.props;
+    const originalMarkdown = getSurveyMarkdown({
+      surveyTitle: surveyMetadata.title,
+      surveyQnaIds,
+      surveyQnasById
+    });
+    var textDiff = this.state.uploadedMarkdown
+      ? this.diff.main(originalMarkdown, this.state.uploadedMarkdown)
+      : null;
+    if (this.state.uploadedMarkdown) this.diff.cleanupSemantic(textDiff);
 
     return (
       <div className="project-survey" id="project-survey">
@@ -48,16 +74,40 @@ export default class UploadInterface extends Component {
         />
         {this.state.uploadedMarkdown ? null : (
           <div className="project-survey__upload-dropzone">
-            <Dropzone onDrop={this.onDrop} multiple={false}>
-              <p>
-                Try dropping some files here, or click to select files to
-                upload.
-              </p>
+            <Dropzone
+              onDrop={this.onDrop}
+              multiple={false}
+              style={{
+                width: "100%",
+                height: "35vh",
+                borderWidth: "0px",
+                background: "#f2f2f2",
+                borderRadius: "5px",
+                color: "#857878",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <h5>drop your markdown file here</h5>
+              <h5>or</h5>
+              <h5>click to select file</h5>
             </Dropzone>
           </div>
         )}
         {this.state.uploadedMarkdown ? (
-          <ReactMarkdown source={this.state.uploadedMarkdown} />
+          <div
+            className="project-survey__upload-diff"
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHtml(this.diff.prettyHtml(textDiff), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+                  "ins",
+                  "del"
+                ])
+              })
+            }}
+          />
         ) : null}
       </div>
     );
