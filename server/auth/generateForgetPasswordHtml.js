f672b0b6ed062e9c530a374b5649cd618f504d6b
yungtiec <tctammychu@gmail.com>
2018-08-01 15:21:57 +0800

    change link url to framework

--- server/auth/generateForgetPasswordHtml.js
+++ server/auth/generateForgetPasswordHtml.js
@@ -419,7 +419,7 @@ module.exports = (inProduction, token) => `<!DOCTYPE html>
 
                                             <a href="https://${
                                               inProduction
-                                                ? "alpha.thebkp.com"
+                                                ? "framework.thebkp.com"
                                                 : "localhost:8000"
                                             }/reset-password/${token}"
                                               target="_blank">Reset your password
