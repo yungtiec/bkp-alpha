498c631bf4ef59989a5618ba685df1de5ea753c7
yungtiec <tctammychu@gmail.com>
2018-09-17 14:36:52 +0800

    spec

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -26,7 +26,10 @@ fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf("spec") === -1
     );
   })
   .forEach(file => {
