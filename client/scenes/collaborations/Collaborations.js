c3637f431274af6708ef90f2723696bd7770c6e4
johnquiwa <johnquiwa@gmail.com>
2018-11-17 00:01:09 -0500

    create regulatory documents section

--- client/scenes/collaborations/Collaborations.js
+++ client/scenes/collaborations/Collaborations.js
@@ -4,20 +4,36 @@ import PropTypes from "prop-types";
 import { ListProject, ListDocumentGrid } from "../../components";
 import { Helmet } from "react-helmet";
 
+const documentTypes = {
+  general: 'general',
+  scorecard: 'scorecard',
+  regulatory: 'regulatory'
+};
+
 const filterDocuments = (documentIds, documentsById) => {
   let scorecardsById = [];
   let scorecardIds = [];
   let thoughtLeadershipById = [];
   let thoughtLeadershipIds = [];
+  let regulatoryById = [];
+  let regulatoryIds = [];
 
   for (let id in documentIds) {
     const docId = documentIds[id];
     const document = documentsById[docId];
     const documentObj = {};
     documentObj[docId] = document;
-    if (document.title.includes("Scorecard")) {
+    if (document.document_type === documentTypes.scorecard) {
       scorecardIds = [].concat(scorecardIds).concat([docId]);
       scorecardsById = Object.assign({}, scorecardsById, documentObj);
+    } else if (document.document_type === documentTypes.regulatory) {
+      console.log(document);
+      regulatoryIds = [].concat(regulatoryIds).concat([docId]);
+      regulatoryById = Object.assign(
+        {},
+        regulatoryById,
+        documentObj
+      );
     } else {
       thoughtLeadershipIds = [].concat(thoughtLeadershipIds).concat([docId]);
       thoughtLeadershipById = Object.assign(
@@ -32,7 +48,9 @@ const filterDocuments = (documentIds, documentsById) => {
     scorecardsById,
     scorecardIds,
     thoughtLeadershipById,
-    thoughtLeadershipIds
+    thoughtLeadershipIds,
+    regulatoryById,
+    regulatoryIds
   };
 };
 
@@ -49,9 +67,11 @@ export default ({
     scorecardsById,
     scorecardIds,
     thoughtLeadershipById,
-    thoughtLeadershipIds
+    thoughtLeadershipIds,
+    regulatoryById,
+    regulatoryIds
   } = filterDocuments(documentIds, documentsById);
-
+  
   return (
     <div>
       <Helmet>
@@ -64,7 +84,7 @@ export default ({
           </div>
           <div className="project-row">
             <div className="projects-containers__collaboration-sub-header d-flex justify-content-between">
-              <div>Thought Leadership</div>
+              <div>Regulatory Guidance</div>
               <button
                 className="btn btn-outline-primary"
                 onClick={() =>
@@ -77,6 +97,15 @@ export default ({
                 Propose collaboration
               </button>
             </div>
+            <ListDocumentGrid
+              documentIds={regulatoryIds}
+              documentsById={regulatoryById}
+            />
+          </div>
+          <div>
+            <span className="projects-containers__collaboration-sub-header d-flex justify-content-between">
+              Thought Leadership
+            </span>
             <ListDocumentGrid
               documentIds={thoughtLeadershipIds}
               documentsById={thoughtLeadershipById}
